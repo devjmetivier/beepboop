5b7dac6f746ea83e677181ddf6faa6059735f264
Devin Metivier <devinmetivier@gmail.com>
2017-09-08 22:32:17 -0400

    deleted some things that were unnecessary

--- app.js
+++ app.js
@@ -35,10 +35,6 @@ const slapp = Slapp({
 const HELP_TEXT = `
 I will respond to the following messages:
 \`help\` - to see this message.
-\`hi\` - to demonstrate a conversation that tracks state.
-\`thanks\` - to demonstrate a simple response.
-\`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
-\`attachment\` - to see a Slack attachment message.
 `;
 
 //*********************************************
@@ -49,51 +45,6 @@ I will respond to the following messages:
 slapp.message('help', ['mention', 'direct_message'], (msg) => {
     msg.say(HELP_TEXT);
 });
-
-// "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
-slapp
-    .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
-        msg
-            .say(`${text}, how are you?`)
-            // sends next event from user to this route, passing along state
-            .route('how-are-you', { greeting: text });
-    })
-    .route('how-are-you', (msg, state) => {
-        let text = (msg.body.event && msg.body.event.text) || '';
-
-        // user may not have typed text as their next action, ask again and re-route
-        if (!text) {
-            return msg
-                .say("Whoops, I'm still waiting to hear how you're doing.")
-                .say('How are you?')
-                .route('how-are-you', state);
-        }
-
-        // add their response to state
-        state.status = text;
-
-        msg
-            .say(`Ok then. What's your favorite color?`)
-            .route('color', state);
-    })
-    .route('color', (msg, state) => {
-        let text = (msg.body.event && msg.body.event.text) || '';
-
-        // user may not have typed text as their next action, ask again and re-route
-        if (!text) {
-            return msg
-                .say("I'm eagerly awaiting to hear your favorite color.")
-                .route('color', state);
-        }
-
-        // add their response to state
-        state.color = text;
-
-        msg
-            .say('Thanks for sharing.')
-            .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``);
-        // At this point, since we don't route anywhere, the "conversation" is over
-    });
 
 // Can use a regex as well
 slapp.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
@@ -119,12 +70,6 @@ slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
             color: '#7CD197'
         }]
     })
-});
-
-slapp.message('spit (.*)', ['direct_mention', 'direct_message'], async (msg, text, match) => {
-    const reply = await snippetController.spit(text, match);
-    msg.say('This is what you said to me:')
-        .say(reply);
 });
 
 slapp.message(/(save|store) (snippet|code)/gi, ['direct_mention', 'direct_message'], async (msg, text) => {
